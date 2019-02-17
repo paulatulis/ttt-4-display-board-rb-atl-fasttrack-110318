5715465106231541d20e751ed76b2becdcc55bab
Paula Tulis <tulis.paula@gmail.com>
2019-02-17 13:52:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
  
 
+ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+ 
  def display_board(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]}"
