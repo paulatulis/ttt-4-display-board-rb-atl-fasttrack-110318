15c72b5393a02d54c0afe084d5372880b8945437
Paula Tulis <tulis.paula@gmail.com>
2019-02-17 13:25:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
- board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+ position = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
- def display_board 
-  puts "   |   |   "
+ def display_board(board)
+  puts " #{position[0]} | #{position[1]} | #{position[2]}"
   puts "-----------"
-  puts "   |   |   "
+  puts " #{position[3]} | #{position[4]} | #{position[5]}"
   puts "-----------"
-  puts "   |   |   "
+  puts " #{position[6]} | #{position[7]} | #{position[8]}"
 end
   
